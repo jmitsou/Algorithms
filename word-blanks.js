4142364b284b557fc80be621e251ca16f4bad468
Jeff Mitsou <shinetachi88@gmail.com>
2021-07-12 20:00:14 -0400

    Initial Commit

---
+++ word-blanks.js
@@ -0,0 +1,10 @@
+// Word Blanks:
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/word-blanks
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = "I " + myVerb + " " + myAdverb + " when the " + myAdjective + " " + myNoun + " chased after me."; // Change this line
+console.log(wordBlanks)
