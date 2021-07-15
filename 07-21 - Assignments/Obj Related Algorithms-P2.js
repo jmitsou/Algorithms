7f0bc7034344e7ec8e71b0733fd8096a5b16e789
Jeff Mitsou <shinetachi88@gmail.com>
2021-07-14 22:03:49 -0400

    Reorganized Folders and added some files

---
+++ 07-21 - Assignments/Obj Related Algorithms-P2.js
@@ -0,0 +1,59 @@
+//Upvotes vs Downvotes
+//https://edabit.com/challenge/654ABGmNS5GqscE8C
+
+function getVoteCount(votes) {
+	
+}
+
+// 50-30-20 Strategy
+// https://edabit.com/challenge/MQL7KSftPQzrxdJw6
+
+
+
+// Profile Lookup
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/profile-lookup
+
+
+
+// Sum of People's Budget
+// https://edabit.com/challenge/tmnCStcrkdWbreKP5
+
+
+
+// Make a Circle with OOP
+// https://edabit.com/challenge/Hgb38yhWGwJCMHbRQ
+
+
+
+// Converting Objects to Arrays
+// https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
+
+
+
+// Wherefore Art Thou
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/wherefore-art-thou
+
+/* function whatIsInAName(collection, source) {
+    let arr = Object.keys(source);
+  // Only change code below this line
+  // .filter - creates a new array that has only the elements that pass the paramaters of the filter 
+  return collection.filter(function(obj) {
+    for (var i = 0; i < arr.length; i++) {
+      if (
+        !obj.hasOwnProperty(arr[i]) ||
+        obj[arr[i]] !== source[arr[i]]
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }
+  ],
+  { last: "Capulet" }
+); */
