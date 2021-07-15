6b1135184c4e33dd67a9821f6c375506b55c564e
Jeff Mitsou <shinetachi88@gmail.com>
2021-07-15 17:56:52 -0400

    Added shopping-list algorithm to Intro-Challanges

---
+++ Intro-Challenges/shopping-list.js
@@ -0,0 +1,6 @@
+// Shopping List:
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/shopping-list
+
+
+var daysOfXmas = [["A partridge", 1], ["turtle doves", 2], ["French hens", 3], ["calling birds", 4], ["golden rings", 5]];
+console.log(daysOfXmas)
