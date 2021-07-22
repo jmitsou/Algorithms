9fef14b936b045465a535dc48c1f19c46337f856
Jeff Mitsou <shinetachi88@gmail.com>
2021-07-21 20:38:43 -0400

    Added Boowho algorithm to Basic Algorithm Scripting

---
+++ Basic-Algorithm-Scripting/Boowho.js
@@ -0,0 +1,7 @@
+//10:Boo who
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/boo-who
+
+ function booWho(bool) {
+    return typeof bool === "boolean";
+  }
+  booWho(true);
